f7cc15287341ddb3c0f4f2990e82419741161290
gustavopelayo-coremedia <gustavo.pelayo@coremedia.com>
2026-02-24 16:40:09 +0000

    training part

---
+++ Starter/tests/Form.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+
+async function fillForm(page, { name = '', email = '', comment = '', checked = false } = {}) {
+    const nameInput = page.getByLabel('Name');
+    const emailInput = page.getByLabel('Email');
+    const checkboxButton = page.getByRole('checkbox');
+    const commentsInput = page.getByLabel('Comment');
+    await nameInput.fill(name);
+    await emailInput.fill(email);
+    await commentsInput.fill(comment);
+    if (checked) {
+        await checkboxButton.check();
+    } else {
+        await checkboxButton.uncheck();
+    }
+}
+
+test('Form is submitted with required fields filled - practice', async ({ page }) => {
+    let formSubmitted = false;
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+        formSubmitted = true;
+    });
+    await page.goto('FeedBackForm.html');
+    await fillForm(page, {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        comment: 'This is a test comment for the feedback form.',
+        checked: true
+    });
+    await page.getByRole('button', { name: 'Submit' }).click();
+    expect(formSubmitted).toBeTruthy();
+});
+
+test('Form is not submitted with required fields empty, form is cleared after submit - practice', async ({ page }) => {
+    let formCleared = false;
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+        formCleared = true;
+    });
+    await page.goto('FeedBackForm.html');
+    await fillForm(page, {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        comment: 'This is a test comment for the feedback form.',
+        checked: true
+    });
+    await page.getByRole('button', { name: 'Clear Progress' }).click();
+    expect(formCleared).toBeTruthy();
+});
+
+test('Form is not submitted with required fields empty - practice', async ({ page }) => {
+    let formSubmitted = false;
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+        formSubmitted = true;
+    });
+    await page.goto('FeedBackForm.html');
+    await fillForm(page, {
+        name: '',
+        email: '',
+        comment: '',
+        checked: false
+    });
+    await page.getByRole('button', { name: 'Submit' }).click();
+    expect(formSubmitted).toBeFalsy();
+});
